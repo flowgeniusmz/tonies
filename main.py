84b4b0f93389c9a9c0f310d11cd7a894389234aa
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-05 16:06:43 +0000

    Initial

---
+++ main.py
@@ -0,0 +1,29 @@
+from pytube import YouTube
+from pydub import AudioSegment
+import os
+
+urls = ["https://www.youtube.com/watch?v=wFnYq2pMDbs","https://www.youtube.com/watch?v=ru0K8uYEZWw", "https://www.youtube.com/watch?v=q0hyYWKXF0Q","https://www.youtube.com/watch?v=Pz2t-yJIp2Q","https://www.youtube.com/watch?v=XqZsoesa55w","https://www.youtube.com/watch?v=dIl8imdPrDE","https://www.youtube.com/watch?v=-ipRfLf79WU"]
+
+#url =  "https://www.youtube.com/watch?v=BHcFQ9gaMF4"
+def get_song(url):
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+
+    # Download the audio file
+    audio_file_path = audio_stream.download()
+
+    # Load the downloaded audio file with pydub (assuming it's an mp4 audio file)
+    audio = AudioSegment.from_file(audio_file_path, format="mp4")
+
+    # Define the path for the mp3 file
+    mp3_file_path = 'songs/' + yt.title + '.mp3'
+
+    # Export the audio to mp3
+    audio.export(mp3_file_path, format="mp3")
+
+    # Optionally, delete the original download to save space
+    os.remove(audio_file_path)
+
+    print(f"Downloaded and converted {yt.title} to MP3.")
+for url in urls:    
+    get_song(url)
